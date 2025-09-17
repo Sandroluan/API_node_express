6cdfa908742e466b43dc03af75206612060de6be
Sandro Luan <67851048+Sandroluan@users.noreply.github.com>
2025-09-16 23:00:44 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const app = express();
+const sql = require('mysql2')
+const port = 3000;
+
+const db = 
+`
+CREATE DATABASE IF NOT EXISTS users;
+
+USE users;
+
+CREATE TABLE IF NOT EXISTS users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nome VARCHAR(55) NOT NULL,
+  email VARCHAR(55) NOT NULL UNIQUE
+)`
+
+
+const connection = sql.createConnection({
+  host: 'localhost',
+  user:'root',
+  password: '',
+  multipleStatements:true
+})
+
+app.use(express.json());
+
+
+
+
+connection.connect((err) => {
+  if(err){
+    console.error('Erro ao conectar ao banco de dados: ', err);
+    return;
+  }
+
+  console.log('conectado ao banco');
+
+  connection.query(db, (err, result)=>{
+    if(err){
+      console.log('erro para criar a tabela')
+      return;
+    }
+    console.log('tabela criada com suscesso')
+
+    app.listen(port, ()=>{
+      console.log(`http://localhost:${port}`)
+    })
+  })
+})
+
+
+
+app.get('/users', (req,res)=>{
+  connection.query('SELECT * FROM users', (err, results) => {
+    if(err){
+      console.error('erro ao buscar usuarios', err);
+      res.status(500).json({error: 'Erro ao buscar usuarios'});
+      return;
+    }
+    res.json(results);
+  })
+})
