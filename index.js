14930eca62a38d558214dd359b5137748747d0b9
Sandro Luan <67851048+Sandroluan@users.noreply.github.com>
2025-09-29 23:35:42 -0300

    update

--- index.js
+++ index.js
@@ -1,63 +1,82 @@
-const express = require('express');
-const app = express();
-const sql = require('mysql2')
+const express = require("express");
+const mysql = require("mysql2");
 const port = 3000;
-
-const db = 
-`
-CREATE DATABASE IF NOT EXISTS users;
-
-USE users;
-
-CREATE TABLE IF NOT EXISTS users (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  nome VARCHAR(55) NOT NULL,
-  email VARCHAR(55) NOT NULL UNIQUE
-)`
-
-
-const connection = sql.createConnection({
-  host: 'localhost',
-  user:'root',
-  password: '',
-  multipleStatements:true
-})
+const app = express();
+const conexao = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "users",
+});
 
 app.use(express.json());
 
+//!pegar todos os usuarios
+app.get("/users", (req, res) => {
+  conexao.query("SELECT * FROM users", (err, result) => {
+    if (err) {
+      return res.status(500).json({"error":"nao foi possivel conectar ao banco de dados"});
+    } else {
+      res.json(result);
+    }
+  });
+});
 
-
-
-connection.connect((err) => {
-  if(err){
-    console.error('Erro ao conectar ao banco de dados: ', err);
-    return;
+//! usuario por id
+app.get('/users/:id', (req,res)=>{
+  const {id} = req.params
+  if (!id){
+    return res.json({error: "daods incompleto 'id' é obrigatorio"})
   }
 
-  console.log('conectado ao banco');
-
-  connection.query(db, (err, result)=>{
-    if(err){
-      console.log('erro para criar a tabela')
-      return;
-    }
-    console.log('tabela criada com sucesso')
-
-    app.listen(port, ()=>{
-      console.log(`http://localhost:${port}`)
+    conexao.query("SELECT * FROM users WHERE id = ?", [id], (err,result)=>{
+    if (err){
+        return res.status(500).json({"erro":"Naõ foi possivel buscar usuario"})
+      }else if (result.length === 0 ){ //! 
+    return res.status(404).json({"mensagem":"usuario nao encontrado"}) 
+      }//!
+      else{
+        console.log('usuario encontrado')
+        res.json(result[0])
+      }
     })
-  })
+    
 })
 
-
-
-app.get('/users', (req,res)=>{
-  connection.query('SELECT * FROM users', (err, results) => {
-    if(err){
-      console.error('erro ao buscar usuarios', err);
-      res.status(500).json({error: 'Erro ao buscar usuarios'});
-      return;
+//! criar usuarios
+app.post("/users", (req, res) => {
+  const { name, cpf, email } = req.body;
+  const values = [name, cpf, email];
+  if (!name || !cpf) {
+    return res.status(400).json({
+      error: "Dados incompletos. 'nome', 'cpf', são obrigatórios.",
+    });
+  }
+  conexao.query(
+    "INSERT INTO users (name, cpf, email) VALUES (?,?,?)",
+    values,
+    (err, result) => {
+      if (err) {
+        return res.status(500).json({"mensagem":"nao foi possivel inserir usuario no banco"});
+      } else {
+        return res.json(result);
+      }
     }
-    res.json(results);
+  );
+});
+
+//! atualizar usuario
+app.patch("/users/:id", (req, res) => {
+  const update = req.body
+  const {id} = req.params
+  const value = [nome, cpf, email]
+  conexao.query('UPDATE users SET nome = ? OR cpf = ? OR email = ? WHERE id = ?' [id],[value], (err,result)=>{
+    if(err){
+      return res.status(500).json({"error":"nao foi possivel alterar"})
+    }
   })
-})
+});
+
+app.listen(port, () => {
+  console.log("http://localhost:" + port);
+});
